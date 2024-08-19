6fe60b75bbf7a68858d57921a7ef566dbc3492ff
Mikael Östberg <mikael.ostberg@rivr.io>
2024-08-19 23:33:44 +0200

    Move projects into samples folder and add SN

---
+++ Samples/Rivr.Samples.Console.DotNet6/MyService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using Rivr;
+
+public class MyService(IClient client) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var health = await client.GetHealthAsync();
+        Console.WriteLine("Result: " + health.Message);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ Samples/Rivr.Samples.Console.DotNet6/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Rivr.Extensions;
+
+Console.WriteLine("Hello, World!");
+
+var host = Host.CreateDefaultBuilder(args).ConfigureServices(
+        (context, services) =>
+        {
+            services.AddRivrClient(context.Configuration, configBuilder =>
+            {
+                configBuilder.UseClientId("enter-client-id-here");
+                configBuilder.UseClientSecret("enter-client-secret-here");
+                configBuilder.UseTestEnvironment();
+
+                /*
+
+                To run this sample, you need to provide your client ID and client secret.
+
+                1. Either use explicit configuration like this:
+
+                configBuilder.UseClientId("enter-client-id-here");
+                configBuilder.UseClientSecret("enter-client-secret-here");
+                configBuilder.UseTestEnvironment();
+
+                2. Or add the following to your appSettings.json:
+
+                "Rivr": {
+                    "ClientId": "enter-client-id-here",
+                    "ClientSecret": "enter-client-secret-here",
+                    "Environment": "Test"
+                }
+
+                The explicit configuration will take precedence.
+
+                */
+            });
+            services.AddHostedService<MyService>();
+        }
+    )
+    .Build();
+
+host.Run();
